ab4f27046a1380c545b18d80a1abd98dff61600d
Ben Trevor <btrevor@8thlight.com>
2014-04-26 10:10:29 -0500

    sorted linked list

--- week_1/homework/lib/sorted_list.rb
+++ week_1/homework/lib/sorted_list.rb
@@ -5,5 +5,16 @@ require_relative '../../in_class/lib/linked_list'
 
 class SortedLinkedList < LinkedList
   def insert(value)
+    index = 0
+    i = 0
+    walk_list do |node|
+      if node.value > value
+        index = i
+        break
+      end
+      i += 1
+    end
+
+    insert_before(index, value)
   end
 end
--- week_1/homework/spec/sorted_list_spec.rb
+++ week_1/homework/spec/sorted_list_spec.rb
@@ -1,4 +1,5 @@
 require 'rspec'
+require 'pry'
 require 'sorted_list'
 
 describe SortedLinkedList do
