081cc77c9685f81b67ba89ce22a28b568fc72355
Ben Trevor <btrevor@8thlight.com>
2014-04-27 21:09:15 -0500

    solution using an array of nodes

--- week_1/in_class/lib/linked_list.rb
+++ week_1/in_class/lib/linked_list.rb
@@ -8,17 +8,44 @@ class LinkedList
     end
   end
 
-  attr_accessor :head
+  attr_accessor :head, :values
+
+  def initialize
+    @values = []
+  end
+
+  def length
+    values.length
+  end
+
+  def head
+    values.first
+  end
 
   def append(value)
+    if head.nil?
+      prepend value
+    else
+      new_node = Node.new(value)
+      values.last.next_node = new_node
+      values << new_node
+    end
   end
 
   def get(index)
+    values[index].value
   end
 
   def prepend(value)
+    original_values = values
+    values.unshift Node.new(value, original_values.first)
   end
 
   def insert_before(index, value)
+    raise Exception, "Out Of Bounds" unless (0..length - 1).include? index
+
+    new_node = Node.new(value, values[index])
+    values[index - 1].next_node = new_node
+    values.insert index, new_node
   end
 end
