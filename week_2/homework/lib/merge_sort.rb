3259d6bff8b9d60690bc66ad6e001e5449d37f71
Ben Trevor <btrevor@8thlight.com>
2014-05-11 14:30:05 -0500

    Optimize, ratio = 4.5

--- week_2/homework/lib/merge_sort.rb
+++ week_2/homework/lib/merge_sort.rb
@@ -1,15 +1,20 @@
 class MergeSort
   def self.merge(left, right)
-    merged = ScoringArray.new(left.length + right.length)
+    left_length = left.length
+    right_length = right.length
     left_index = right_index = merged_index = 0
 
-    until left_index == left.length and right_index == right.length
+    merged = ScoringArray.new(left_length + right_length)
 
-      if right_index == right.length or (left_index != left.length and left.get(left_index) < right.get(right_index))
-        merged.set(merged_index, left.get(left_index))
+    until left_index == left_length and right_index == right_length
+      left_val = left.get(left_index)
+      right_val = right.get(right_index)
+
+      if right_index == right_length or (left_index != left_length and left_val < right_val)
+        merged.set(merged_index, left_val)
         left_index += 1
       else
-        merged.set(merged_index, right.get(right_index))
+        merged.set(merged_index, right_val)
         right_index += 1
       end
 
@@ -20,11 +25,12 @@ class MergeSort
   end
 
   def self.sort(to_sort)
-    return to_sort if to_sort.length == 1
+    to_sort_length = to_sort.length
+    return to_sort if to_sort_length == 1
 
-    midpoint = (to_sort.length / 2) - 1
+    midpoint = (to_sort_length / 2) - 1
     left = to_sort.slice(0, midpoint)
-    right = to_sort.slice(midpoint + 1, to_sort.length)
+    right = to_sort.slice(midpoint + 1, to_sort_length)
 
     merge(sort(left), sort(right))
   end
