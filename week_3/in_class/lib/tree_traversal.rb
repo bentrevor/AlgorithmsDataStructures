bde05127efca3b5aabc684c6aac894b58b979b58
Ben Trevor <btrevor@8thlight.com>
2014-05-25 11:39:27 -0500

    breadth-first and depth-first search

--- week_3/in_class/lib/tree_traversal.rb
+++ week_3/in_class/lib/tree_traversal.rb
@@ -6,10 +6,22 @@ class TreeNode
     self.children = []
   end
 
-  def depth_first_flat_list
+  def depth_first_flat_list(list=[])
+    list << value
+
+    children.each do |child|
+      child.depth_first_flat_list(list)
+    end
+
+    list
   end
 
-  def breadth_first_flat_list
+  def breadth_first_flat_list(list=[], roots=[self])
+    return list if roots.empty?
+
+    list << roots.map(&:value)
+    new_roots = roots.map(&:children)
+
+    breadth_first_flat_list(list.flatten, new_roots.flatten)
   end
 end
-
