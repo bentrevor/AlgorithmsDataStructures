51ee087b6f268ad20d1c93c4e5cbcf144ad38e47
Ben Trevor <btrevor@8thlight.com>
2014-06-14 14:46:59 -0500

    function to print out tree

--- week_3/in_class/lib/tree_traversal.rb
+++ week_3/in_class/lib/tree_traversal.rb
@@ -6,14 +6,10 @@ class TreeNode
     self.children = []
   end
 
-  def depth_first_flat_list(list=[])
-    list << value
+  def depth_first_flat_list(list=[], depth=0)
+    add_to_flat_list = Proc.new { |acc, _| acc << value }
 
-    children.each do |child|
-      child.depth_first_flat_list(list)
-    end
-
-    list
+    depth_first_traversal(list, add_to_flat_list, :depth_first_flat_list, 0)
   end
 
   def breadth_first_flat_list(list=[], roots=[self])
@@ -23,5 +19,28 @@ class TreeNode
     new_roots = roots.map(&:children)
 
     breadth_first_flat_list(list.flatten, new_roots.flatten)
+  end
+
+  def printable(printed_tree="\n\n", depth=0)
+    add_next_level = Proc.new do |acc, depth|
+      indentation = "  " * depth
+      next_level = indentation + "#{value}\n"
+
+      acc << next_level
+    end
+
+    depth_first_traversal(printed_tree, add_next_level, :printable, depth + 1)
+  end
+
+  private
+
+  def depth_first_traversal(accumulator, add_to_accumulator, iterating_function, depth)
+    add_to_accumulator.call(accumulator, depth)
+
+    children.each do |child|
+      child.send iterating_function, accumulator, depth
+    end
+
+    accumulator
   end
 end
--- week_3/in_class/spec/tree_traversal_spec.rb
+++ week_3/in_class/spec/tree_traversal_spec.rb
@@ -27,4 +27,26 @@ describe TreeNode do
 
     expect(root.breadth_first_flat_list).to eq([42, 2, 8, 99, 76, 0, 7, 999])
   end
+
+  it 'builds a string to print the tree values' do
+    root = TreeNode.new(42)
+        root.children << TreeNode.new(2)
+            root.children[0].children << TreeNode.new(99)
+            root.children[0].children << TreeNode.new(76)
+                root.children[0].children[1].children << TreeNode.new(999)
+        root.children << TreeNode.new(8)
+            root.children[1].children << TreeNode.new(0)
+            root.children[1].children << TreeNode.new(7)
+
+    printed_tree = root.printable
+
+    printed_tree.should include "42\n"
+    printed_tree.should include "  2\n"
+    printed_tree.should include "    99\n"
+    printed_tree.should include "    76\n"
+    printed_tree.should include "      999\n"
+    printed_tree.should include "  8\n"
+    printed_tree.should include "    0\n"
+    printed_tree.should include "    7\n"
+  end
 end
